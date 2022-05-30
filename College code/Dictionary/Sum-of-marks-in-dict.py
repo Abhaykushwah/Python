7a4583163c387f1c67660b37217d3b661cfd17d6
Abhay Kushwah <67948225+Abhaykushwah@users.noreply.github.com>
2022-05-30 11:52:20 +0530

    Create Sum-of-marks-in-dict.py

---
+++ College code/Dictionary/Sum-of-marks-in-dict.py
@@ -0,0 +1,19 @@
+std = {}
+std_count = int(input("data of how many studends : "))
+
+for i in range(std_count):
+        k1= int(input("Enter the roll number : "))
+        dict_sub = {}
+        for j in range(5):
+
+                k2=input("enter the subject name : ")
+                v2=int(input("Enter the marks in subject : "))
+                dict_sub[k2]=v2
+        std[k1]=dict_sub
+print(std)
+###printing the results
+sum_of_marks = 0
+for i in std:
+        for j in std[i]:
+                sum_of_marks += std[i][j]
+        print("Total number you got in the exam ",sum_of_marks)
