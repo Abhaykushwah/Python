4f1f6eabfa5d1d0396e65e24db9031a16fbab2c6
Abhay Kushwah <67948225+Abhaykushwah@users.noreply.github.com>
2022-05-31 20:39:00 +0530

    Adding comments

--- College code/Dictionary/finding max-min.py
+++ College code/Dictionary/finding max-min.py
@@ -1,7 +1,10 @@
+# dict init
 my_dict = {'x':500, 'y':5874, 'z': 560}
 
+# finding the key of min and Max value
 key_max = max(my_dict.keys(), key=(lambda k: my_dict[k]))
 key_min = min(my_dict.keys(), key=(lambda k: my_dict[k]))
 
+## printing result
 print('Maximum Value: ',my_dict[key_max])
 print('Minimum Value: ',my_dict[key_min])
